3e8b4157d97bbc17d8d8ef339fd249ced4fca591
Ebram Shehata <ebram96@gmail.com>
2018-01-29 19:51:52 +0200

    Add problem 10074

---
+++ UVa_Solutions/UVa 10074 - Take the Land/UVa 10074 - Take the Land.cpp
@@ -0,0 +1,72 @@
+//It's all about what U BELIEVE
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<bitset>
+#include<stdio.h>
+#include<map>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<cstring>
+#include<cmath>
+#include<set>
+#define INF ~(1<<31)
+#define endl '\n'
+#define fo(s,y,z) for(int y=s ; y<(int)z ; y++)
+#define pb push_back
+#define pob pop_back
+#define gcu getchar_unlocked
+#define allof(Z) Z.begin(),Z.end()
+#define modulo 1000000007
+#define wtm while(t--)
+#define wnm while(n--)
+#define lne if(line)puts("");else line =1
+#define lsone(Z) (Z&-Z)
+#define readf freopen("/home/ebram96/Desktop/in" , "r" , stdin);
+#define writef freopen("/home/ebram96/Desktop/out" , "w" , stdout);
+using namespace std;
+typedef vector<int> vi;
+typedef vector<string> vstr;
+typedef unsigned int ui;
+typedef unsigned long long ull;
+typedef long long ll;
+typedef pair<int,int> pairii ;
+typedef pair<ull,ull> pairull;
+typedef set<int> seti;
+typedef set<ull> setull;
+typedef queue<int> qint;
+typedef deque<int> dqint;
+//int dx[]={-1,-1,0,1,1, 1, 0,-1};
+//int dy[]={ 0, 1,1,1,0,-1,-1,-1};
+//int dx[]={-1,0,1, 0};
+//int dy[]={ 0,1,0,-1};
+int main()
+{
+	//readf
+	int n , m , res , a[100][100] , sum;
+	while(scanf("%d %d",&n,&m)&&n)
+	{
+		fo(0,x,n)fo(0,y,m)
+		{
+			scanf("%d",&a[x][y]);
+			a[x][y] = !a[x][y];
+			if(x&&y)a[x][y]-=a[x-1][y-1];
+			if(x)a[x][y]+=a[x-1][y];
+			if(y)a[x][y]+=a[x][y-1];
+		}
+		sum = res = 0;
+		fo(0,x,n)fo(0,y,m)
+			fo(x,downx,n)fo(y,downy,m)
+			{
+				sum = a[downx][downy];
+				if(x&&y)sum+=a[x-1][y-1];
+				if(x)sum-=a[x-1][downy];
+				if(y)sum-=a[downx][y-1];
+				if((sum>res)&&(sum==(downx-x+1)*(downy-y+1)))
+					res = sum;
+			}
+		printf("%d\n",res);
+	}
+}
+
