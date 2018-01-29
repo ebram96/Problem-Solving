7417c13dba5e56a0b098e429aa8134e088714b80
Ebram Shehata <ebram96@gmail.com>
2018-01-29 19:51:35 +0200

    Add problem 10063

---
+++ UVa_Solutions/UVa 10063 - Knuth’s Permutation/UVa 10063 - Knuth’s Permutation.cpp
@@ -0,0 +1,75 @@
+//It's all about what U BELIEVE
+#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<bitset>
+#include<stdio.h>
+#include<map>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<cstring>
+#include<cmath>
+#include<set>
+#define INF ~(1<<31)
+#define endl '\n'
+#define fo(s , y , z) for(int y = s ; y < (int)z ; y++)
+#define pb push_back
+#define pob pop_back
+#define gcu getchar_unlocked
+#define modulo 1000000007
+#define wtm while(t--)
+#define wnm while(n--)
+#define lne if(line)puts("");else line =1
+#define lsone(Z) (Z&-Z)
+#define readf freopen("/home/ebram96/Desktop/in" , "r" , stdin);
+#define writef freopen("/home/ebram96/Desktop/out" , "w" , stdout);
+using namespace std;
+typedef vector<int> vi;
+typedef vector<string> vstr;
+typedef unsigned int ui;
+typedef unsigned long long ull;
+typedef long long ll;
+typedef pair<int,int> pairii ;
+typedef pair<ull,ull> pairull;
+typedef set<int> seti;
+typedef set<ull> setull;
+typedef queue<int> qint;
+typedef deque<int> dqint;
+//int dx[]={-1,-1,0,1,1, 1, 0,-1};
+//int dy[]={ 0, 1,1,1,0,-1,-1,-1};
+//int dx[]={-1,0,1, 0};
+//int dy[]={ 0,1,0,-1};
+char s[10] , perm[10];
+int len;
+bool line;
+void generate(int i)
+{
+	if(i==len)
+	{
+		puts(perm);
+		return;
+	}
+	for(int y = i ; y>=1 ; y--)
+		perm[y] = perm[y-1];
+	fo(0,y,i+1)
+	{
+		perm[y] = s[i];
+		generate(i+1);
+		perm[y] = perm[y+1];
+	}
+}
+int main()
+{
+	//readf
+	while(scanf("%s",s)==1)
+	{
+		lne;
+		line = 1;
+		len = strlen(s);
+		perm[len] = 0;
+		generate(0);
+	}
+}
+
