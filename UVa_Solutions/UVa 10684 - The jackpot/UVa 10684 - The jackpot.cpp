a38ad70ed6d91bdb10b5363653628dbe41b24014
Ebram Shehata <ebram96@gmail.com>
2018-01-30 17:51:17 +0200

    Add problem 10684

---
+++ UVa_Solutions/UVa 10684 - The jackpot/UVa 10684 - The jackpot.cpp
@@ -0,0 +1,62 @@
+//It's all about what U BELIEVE
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<bitset>
+#include<stdio.h>
+#include<map>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<cstring>
+#include<cmath>
+#include<set>
+#define INF ~(1<<31)
+#define endl '\n'
+#define fo(s,y,z) for(int y=s ; y<(int)z ; y++)
+#define pb push_back
+#define pob pop_back
+#define gcu getchar_unlocked
+#define allof(Z) Z.begin(),Z.end()
+#define modulo 1000000007
+#define wtm while(t--)
+#define wnm while(n--)
+#define lne if(line)puts("");else line =1
+#define lsone(Z) (Z&-Z)
+#define readf freopen("/home/ebram96/Desktop/in" , "r" , stdin);
+#define writef freopen("/home/ebram96/Desktop/out" , "w" , stdout);
+using namespace std;
+typedef vector<int> vi;
+typedef vector<string> vstr;
+typedef unsigned int ui;
+typedef unsigned long long ull;
+typedef long long ll;
+typedef pair<int,int> pairii ;
+typedef pair<ull,ull> pairull;
+typedef set<int> seti;
+typedef set<ull> setull;
+typedef queue<int> qint;
+typedef deque<int> dqint;
+//int dx[]={-1,-1,0,1,1, 1, 0,-1};
+//int dy[]={ 0, 1,1,1,0,-1,-1,-1};
+//int dx[]={-1,0,1, 0};
+//int dy[]={ 0,1,0,-1};
+int main()
+{
+	//readf
+	int n , inp , mx_here , mx_tot;
+	while(scanf("%d",&n)&&n)
+	{
+		scanf("%d",&inp);
+		mx_tot = mx_here = inp;
+		fo(0,y,n-1)
+		{
+			scanf("%d",&inp);
+			mx_here = max(mx_here+inp,inp);
+			if(mx_here>mx_tot)mx_tot = mx_here;
+		}
+		if(mx_tot>0)printf("The maximum winning streak is %d.\n",mx_tot);
+		else puts("Losing streak.");
+	}
+}
+
