f7e95206f76a6ff4bc08641db25d005c4582e4e3
Ebram Shehata <ebram96@gmail.com>
2018-02-04 17:01:59 +0200

    Add problem 11059

---
+++ UVa_Solutions/UVa 11059 - Maximum Product/UVa 11059 - Maximum Product.cpp
@@ -0,0 +1,63 @@
+//It's all about what U BELIEVE
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<bitset>
+#include<stdio.h>
+#include<map>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<cstring>
+#include<cmath>
+#include<set>
+#define endl '\n'
+#define fo(s , y , z) for(int y = s ; y < (int)z ; y++)
+#define pb push_back
+#define pob pop_back
+#define gcu getchar_unlocked
+#define modulo 1000000007
+#define wtm while(t--)
+#define wnm while(n--)
+#define lne if(line)puts("");else line =1
+#define lsone(Z) (Z&-Z)
+#define readf freopen("/home/ebram96/Desktop/in" , "r" , stdin);
+#define writef freopen("/home/ebram96/Desktop/out" , "w" , stdout);
+using namespace std;
+typedef vector<int> vi;
+typedef vector<string> vstr;
+typedef unsigned int ui;
+typedef unsigned long long ull;
+typedef long long ll;
+typedef pair<int,int> pairii ;
+typedef pair<ull,ull> pairull;
+typedef set<int> seti;
+typedef set<ull> setull;
+typedef queue<int> qint;
+typedef deque<int> dqint;
+//int dx[]={-1,-1,0,1,1, 1, 0,-1};
+//int dy[]={ 0, 1,1,1,0,-1,-1,-1};
+//int dx[]={-1,0,1, 0};
+//int dy[]={ 0,1,0,-1};
+int main()
+{
+	//readf
+	int n, c = 1 , a[18];
+	ll res , prod ;
+	while(scanf("%d",&n)==1)
+	{
+		res = 0;
+		fo(0,y,n)scanf("%d",&a[y]);
+		fo(0,y,n)
+		{
+			prod = 1;
+			fo(y,i,n)
+			{
+				prod*=a[i];
+				res = max(res,prod);
+			}
+		}
+		printf("Case #%d: The maximum product is %lld.\n\n",c++,res);
+	}
+}
+
