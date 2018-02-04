7798d13874f1b2d12ad77dba0cd5a26f9852aebd
Ebram Shehata <ebram96@gmail.com>
2018-02-04 16:59:30 +0200

    Add problem 10943

---
+++ UVa_Solutions/UVa 10943 - How do you add?/UVa 10943 - How do you add?.cpp
@@ -0,0 +1,64 @@
+//It's all about what U BELIEVE
+#include<algorithm>
+#include<iostream>
+#include<stdio.h>
+#include<cstring>
+#include<vector>
+#include<bitset>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<cmath>
+#include<map>
+#include<set>
+#define endl '\n'
+#define PI acos(-1)
+#define INF ~(1<<31)
+#define pb push_back
+#define pob pop_back
+#define wtm while(t--)
+#define wnm while(n--)
+#define lsone(Z) (Z&-Z)
+#define modulo 1000000007
+#define gcu getchar_unlocked
+#define allof(Z) Z.begin(),Z.end()
+#define lne if(line)puts("");else line =1
+#define fo(s,y,z) for(int y=s ; y<(int)z ; y++)
+#define readf freopen("/home/ebram96/Desktop/in" , "r" , stdin);
+#define writef freopen("/home/ebram96/Desktop/out" , "w" , stdout);
+using namespace std;
+typedef unsigned long long ull;
+typedef pair<ull,ull> pairull;
+typedef pair<int,int> pairii;
+typedef vector<string> vstr;
+typedef deque<int> dqint;
+typedef set<ull> setull;
+typedef unsigned int ui;
+typedef queue<int> qint;
+typedef vector<int> vi;
+typedef set<int> seti;
+typedef long long ll;
+//int dx[]={-1,-1,0,1,1, 1, 0,-1};
+//int dy[]={ 0, 1,1,1,0,-1,-1,-1};
+//int dx[]={-1,0,1, 0};
+//int dy[]={ 0,1,0,-1};
+int mars[101][101];
+int rec(int n,int k)
+{
+	if(k==1)return 1;
+	int &ret = mars[n][k];
+	if(~ret)return ret;
+	ret = 0;
+	fo(0,y,n+1)
+		ret+=rec(n-y,k-1)%1000000;
+	return ret;
+}
+int main()
+{
+	//readf
+	int n , k;
+	memset(mars,-1,sizeof(mars));
+	while(scanf("%d %d",&n,&k)&&n)
+		printf("%d\n",rec(n,k)%1000000);
+}
+
