07957dca31bc87e90b77dfb10da3ff460fb61c7b
Ebram Shehata <ebram96@gmail.com>
2018-02-05 11:26:34 +0200

    Add problem 11084

---
+++ UVa_Solutions/UVa 11084 - Anagram Division/UVa 11084 - Anagram Division.cpp
@@ -0,0 +1,70 @@
+//It's all about what U BELIEVE
+#include<algorithm>
+#include<iostream>
+#include<stdio.h>
+#include<cstring>
+#include<vector>
+#include<bitset>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<cmath>
+#include<map>
+#include<set>
+#define endl '\n'
+#define INF ~(1<<31)
+#define pb push_back
+#define pob pop_back
+#define wtm while(t--)
+#define wnm while(n--)
+#define lsone(Z) (Z&-Z)
+#define modulo 1000000007
+#define gcu getchar_unlocked
+#define allof(Z) Z.begin(),Z.end()
+#define lne if(line)puts("");else line =1
+#define fo(s,y,z) for(int y=s ; y<(int)z ; y++)
+#define readf freopen("/home/ebram96/Desktop/in" , "r" , stdin);
+#define writef freopen("/home/ebram96/Desktop/out" , "w" , stdout);
+using namespace std;
+typedef unsigned long long ull;
+typedef pair<ull,ull> pairull;
+typedef pair<int,int> pairii;
+typedef vector<string> vstr;
+typedef deque<int> dqint;
+typedef set<ull> setull;
+typedef unsigned int ui;
+typedef queue<int> qint;
+typedef vector<int> vi;
+typedef set<int> seti;
+typedef long long ll;
+//int dx[]={-1,-1,0,1,1, 1, 0,-1};
+//int dy[]={ 0, 1,1,1,0,-1,-1,-1};
+//int dx[]={-1,0,1, 0};
+//int dy[]={ 0,1,0,-1};
+int main()
+{
+	//readf
+	char s[11];
+	int t , d , res , len;
+	scanf("%d",&t);
+	wtm
+	{
+		scanf("%s %d",s,&d);
+		res = 0;
+		len = strlen(s);
+		sort(s,s+len);
+		do
+		{
+			ull val = 0;
+			fo(0,y,len)
+			{
+				val*=10;
+				val+=(s[y]-'0');
+			}
+			res+=(val%d==0);
+		}
+		while(next_permutation(s,s+strlen(s)));
+		printf("%d\n",res);
+	}
+}
+
