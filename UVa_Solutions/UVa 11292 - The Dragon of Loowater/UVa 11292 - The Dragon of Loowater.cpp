79e493be8e7b4d83669be2b272cc254950d5cee2
Ebram Shehata <ebram96@gmail.com>
2018-02-08 13:02:38 +0200

    Add problem 11292

---
+++ UVa_Solutions/UVa 11292 - The Dragon of Loowater/UVa 11292 - The Dragon of Loowater.cpp
@@ -0,0 +1,65 @@
+//It's all about what U BELIEVE
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<bitset>
+#include<stdio.h>
+#include<map>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<cstring>
+#include<cmath>
+#include<set>
+#define INF ~(1<<31)
+#define endl '\n'
+#define fo(s,y,z) for(int y=s ; y<(int)z ; y++)
+#define pb push_back
+#define pob pop_back
+#define gcu getchar_unlocked
+#define allof(Z) Z.begin(),Z.end()
+#define modulo 1000000007
+#define wtm while(t--)
+#define wnm while(n--)
+#define lne if(line)puts("");else line =1
+#define lsone(Z) (Z&-Z)
+#define readf freopen("/home/ebram96/Desktop/in" , "r" , stdin);
+#define writef freopen("/home/ebram96/Desktop/out" , "w" , stdout);
+using namespace std;
+typedef vector<int> vi;
+typedef vector<string> vstr;
+typedef unsigned int ui;
+typedef unsigned long long ull;
+typedef long long ll;
+typedef pair<int,int> pairii ;
+typedef pair<ull,ull> pairull;
+typedef set<int> seti;
+typedef set<ull> setull;
+typedef queue<int> qint;
+typedef deque<int> dqint;
+//int dx[]={-1,-1,0,1,1, 1, 0,-1};
+//int dy[]={ 0, 1,1,1,0,-1,-1,-1};
+//int dx[]={-1,0,1, 0};
+//int dy[]={ 0,1,0,-1};
+int main()
+{
+	//readf
+	int n , m , res , knight[20000] , head[20000] , ik , ih;
+	while(scanf("%d %d",&n,&m)&&n)
+	{
+		fo(0,y,n)scanf("%d",&head[y]);
+		fo(0,y,m)scanf("%d",&knight[y]);
+		sort(head,head+n);
+		sort(knight,knight+m);
+		ik = ih = res = 0;
+		while(ik<m&&ih<n)
+		{
+			if(head[ih]<=knight[ik])
+				res+=knight[ik] , ih++;
+			ik++;
+		}
+		if(ih!=n)puts("Loowater is doomed!");
+		else printf("%d\n",res);
+	}
+}
+
