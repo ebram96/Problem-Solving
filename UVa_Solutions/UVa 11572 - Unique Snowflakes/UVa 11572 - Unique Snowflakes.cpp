b0f16f051ec7373535aeb00d8ec6ce3b1be6eb1c
Ebram Shehata <ebram96@gmail.com>
2018-02-09 10:17:29 +0200

    Add problem 11572

---
+++ UVa_Solutions/UVa 11572 - Unique Snowflakes/UVa 11572 - Unique Snowflakes.cpp
@@ -0,0 +1,71 @@
+//It's all about what U BELIEVE
+#include<algorithm>
+#include<iostream>
+#include<stdio.h>
+#include<cstring>
+#include<vector>
+#include<bitset>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<cmath>
+#include<map>
+#include<set>
+#define endl '\n'
+#define PI acos(-1)
+#define INF ~(1<<31)
+#define pb push_back
+#define pob pop_back
+#define lsone(Z) (Z&-Z)
+#define modulo 1000000007
+#define gcu getchar_unlocked
+#define allof(Z) Z.begin(),Z.end()
+#define rallof(Z) Z.rbegin(),Z.rend()
+#define mset(z,v) memset(z,v,sizeof(z))
+#define lne if(line)puts("");else line =1
+#define fo(s,y,z) for(int y=s ; y<(int)z ; y++)
+#define readf freopen("/home/ebram96/Desktop/in" , "r" , stdin);
+#define writef freopen("/home/ebram96/Desktop/out" , "w" , stdout);
+using namespace std;
+typedef unsigned long long ull;
+typedef pair<ull,ull> pairull;
+typedef pair<int,int> pairii;
+typedef vector<string> vstr;
+typedef deque<int> dqint;
+typedef set<ull> setull;
+typedef unsigned int ui;
+typedef queue<int> qint;
+typedef vector<int> vi;
+typedef set<int> seti;
+typedef long long ll;
+//int dx[]={-1,0,1, 0,-1,1, 1,-1};
+//int dy[]={ 0,1,0,-1, 1,1,-1,-1};
+int main()
+{
+	//readf
+	seti package;
+	int t , n , res , first_appearance , a[1000000];
+	scanf("%d",&t);
+	while(t--)
+	{
+		scanf("%d",&n);
+		package.clear();
+		res = first_appearance = 0;
+		fo(0,y,n)
+		{
+			scanf("%d",&a[y]);
+			if(package.find(a[y])!=package.end())
+			{
+				while(a[first_appearance]!=a[y])
+					first_appearance++;
+				res = max(res,int(package.size()));
+				package = seti(a+first_appearance,a+y);
+				first_appearance++;
+			}
+			else package.insert(a[y]);
+		}
+		res = max(res,int(package.size()));
+		printf("%d\n",res);
+	}
+}
+
