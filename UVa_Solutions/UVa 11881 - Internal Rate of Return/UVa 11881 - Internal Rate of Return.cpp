d0f8c8bac7d70d7bf4cbb8f39266a3382f8efc08
Ebram Shehata <ebram96@gmail.com>
2018-02-11 15:23:40 +0200

    Add problem 11881

---
+++ UVa_Solutions/UVa 11881 - Internal Rate of Return/UVa 11881 - Internal Rate of Return.cpp
@@ -0,0 +1,65 @@
+//It's all about what U BELIEVE
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<bitset>
+#include<stdio.h>
+#include<map>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<cstring>
+#include<cmath>
+#include<set>
+#define INF ~(1<<31)
+#define endl '\n'
+#define fo(s,y,z) for(int y=s ; y<(int)z ; y++)
+#define pb push_back
+#define pob pop_back
+#define gcu getchar_unlocked
+#define allof(Z) Z.begin(),Z.end()
+#define modulo 1000000007
+#define wtm while(t--)
+#define wnm while(n--)
+#define lne if(line)puts("");else line =1
+#define lsone(Z) (Z&-Z)
+#define readf freopen("/home/ebram96/Desktop/in" , "r" , stdin);
+#define writef freopen("/home/ebram96/Desktop/out" , "w" , stdout);
+using namespace std;
+typedef vector<int> vi;
+typedef vector<string> vstr;
+typedef unsigned int ui;
+typedef unsigned long long ull;
+typedef long long ll;
+typedef pair<int,int> pairii ;
+typedef pair<ull,ull> pairull;
+typedef set<int> seti;
+typedef set<ull> setull;
+typedef queue<int> qint;
+typedef deque<int> dqint;
+//int dx[]={-1,-1,0,1,1, 1, 0,-1};
+//int dy[]={ 0, 1,1,1,0,-1,-1,-1};
+//int dx[]={-1,0,1, 0};
+//int dy[]={ 0,1,0,-1};
+int main()
+{
+	//readf
+	int t , cf[11];
+	while(scanf("%d",&t)&&t)
+	{
+		fo(0,y,t+1)scanf("%d",&cf[y]);
+		double lo = -1.0+1e-9 , hi = 10000.0 , mid , fx;
+		int loop = 100;
+		while(loop--)
+		{
+			mid = (lo+hi)/2.0;
+			fx = cf[0];
+			fo(1,y,t+1)
+				fx+=cf[y]/pow(1.0+mid,y);
+			if(fx<=0)hi = mid;
+			else lo = mid;
+		}
+		printf("%.2lf\n",mid);
+	}
+}
+
