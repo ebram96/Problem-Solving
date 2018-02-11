82537713b77381dcdf8ef3376b7a789dbeb0b390
Ebram Shehata <ebram96@gmail.com>
2018-02-11 15:21:50 +0200

    Add problem 11703

---
+++ UVa_Solutions/UVa 11703 - sqrt log sin/UVa 11703 - sqrt log sin.cpp
@@ -0,0 +1,65 @@
+//It's all about what U BELIEVE
+#include<algorithm>
+#include<iostream>
+#include<stdio.h>
+#include<cstring>
+#include<vector>
+#include<bitset>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<cmath>
+#include<map>
+#include<set>
+#define endl '\n'
+#define PI acos(-1)
+#define INF ~(1<<31)
+#define pb push_back
+#define pob pop_back
+#define wtm while(t--)
+#define wnm while(n--)
+#define lsone(Z) (Z&-Z)
+#define modulo 1000000007
+#define gcu getchar_unlocked
+#define allof(Z) Z.begin(),Z.end()
+#define lne if(line)puts("");else line =1
+#define fo(s,y,z) for(int y=s ; y<(int)z ; y++)
+#define readf freopen("/home/ebram96/Desktop/in" , "r" , stdin);
+#define writef freopen("/home/ebram96/Desktop/out" , "w" , stdout);
+using namespace std;
+typedef unsigned long long ull;
+typedef pair<ull,ull> pairull;
+typedef pair<int,int> pairii;
+typedef vector<string> vstr;
+typedef deque<int> dqint;
+typedef set<ull> setull;
+typedef unsigned int ui;
+typedef queue<int> qint;
+typedef vector<int> vi;
+typedef set<int> seti;
+typedef long long ll;
+//int dx[]={-1,0,1, 0};
+//int dy[]={ 0,1,0,-1};
+//int dx[]={-1,-1,0,1,1, 1, 0,-1};
+//int dy[]={ 0, 1,1,1,0,-1,-1,-1};
+int seq[1000001] , ind;
+void compute()
+{
+	int rt , ln , sn;
+	fo(1,i,1000001)
+	{
+		rt = i-sqrt(i);
+		ln = log(i);
+		sn = i*sin(i)*sin(i);
+		seq[i] = (seq[rt]+seq[ln]+seq[sn])%1000000;
+	}
+}
+int main()
+{
+	//readf
+	seq[0] = 1;
+	compute();
+	while(scanf("%d",&ind)&&~ind)
+		printf("%d\n",seq[ind]);
+}
+
