cb0877ac96c1be334f3fabccb2af5d9226490968
Ebram Shehata <ebram96@gmail.com>
2018-03-11 23:57:06 +0200

    Add problem 12210

---
+++ UVa_Solutions/UVa 12210 - A Match Making Problem/UVa 12210 - A Match Making Problem.cpp
@@ -0,0 +1,61 @@
+//It's all about what U BELIEVE
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<bitset>
+#include<stdio.h>
+#include<map>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<cstring>
+#include<cmath>
+#include<set>
+#define INF ~(1<<31)
+#define endl '\n'
+#define fo(s,y,z) for(int y=s ; y<(int)z ; y++)
+#define pb push_back
+#define pob pop_back
+#define gcu getchar_unlocked
+#define allof(Z) Z.begin(),Z.end()
+#define modulo 1000000007
+#define wtm while(t--)
+#define wnm while(n--)
+#define lne if(line)puts("");else line =1
+#define lsone(Z) (Z&-Z)
+#define readf freopen("/home/ebram96/Desktop/in" , "r" , stdin);
+#define writef freopen("/home/ebram96/Desktop/out" , "w" , stdout);
+using namespace std;
+typedef vector<int> vi;
+typedef vector<string> vstr;
+typedef unsigned int ui;
+typedef unsigned long long ull;
+typedef long long ll;
+typedef pair<int,int> pairii ;
+typedef pair<ull,ull> pairull;
+typedef set<int> seti;
+typedef set<ull> setull;
+typedef queue<int> qint;
+typedef deque<int> dqint;
+//int dx[]={-1,-1,0,1,1, 1, 0,-1};
+//int dy[]={ 0, 1,1,1,0,-1,-1,-1};
+//int dx[]={-1,0,1, 0};
+//int dy[]={ 0,1,0,-1};
+int main()
+{
+	//readf
+	int b , s , mn , inp , c = 0;
+	while(scanf("%d %d",&b,&s)&&b)
+	{
+		mn = INF;
+		fo(0,y,b)
+		{
+			scanf("%d",&inp);
+			mn = min(mn , inp);
+		}
+		fo(0,y,s)scanf("%d",&inp);
+		if(s>=b)printf("Case %d: 0\n",++c);
+		else printf("Case %d: %d %d\n",++c,b-s,mn);
+	}
+}
+
