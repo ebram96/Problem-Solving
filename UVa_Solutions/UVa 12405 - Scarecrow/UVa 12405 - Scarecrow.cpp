8d5ad0b837f347f86984d94967087c8beaab072d
Ebram Shehata <ebram96@gmail.com>
2018-03-11 23:58:01 +0200

    Add problem 12405

---
+++ UVa_Solutions/UVa 12405 - Scarecrow/UVa 12405 - Scarecrow.cpp
@@ -0,0 +1,66 @@
+//It's all about what U BELIEVE
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<bitset>
+#include<stdio.h>
+#include<map>
+#include<stack>
+#include<queue>
+#include<deque>
+#include<cstring>
+#include<cmath>
+#include<set>
+#define INF ~(1<<31)
+#define endl '\n'
+#define fo(s,y,z) for(int y=s ; y<(int)z ; y++)
+#define pb push_back
+#define pob pop_back
+#define gcu getchar_unlocked
+#define allof(Z) Z.begin(),Z.end()
+#define modulo 1000000007
+#define wtm while(t--)
+#define wnm while(n--)
+#define lne if(line)puts("");else line =1
+#define lsone(Z) (Z&-Z)
+#define readf freopen("/home/ebram96/Desktop/in" , "r" , stdin);
+#define writef freopen("/home/ebram96/Desktop/out" , "w" , stdout);
+using namespace std;
+typedef vector<int> vi;
+typedef vector<string> vstr;
+typedef unsigned int ui;
+typedef unsigned long long ull;
+typedef long long ll;
+typedef pair<int,int> pairii ;
+typedef pair<ull,ull> pairull;
+typedef set<int> seti;
+typedef set<ull> setull;
+typedef queue<int> qint;
+typedef deque<int> dqint;
+//int dx[]={-1,-1,0,1,1, 1, 0,-1};
+//int dy[]={ 0, 1,1,1,0,-1,-1,-1};
+//int dx[]={-1,0,1, 0};
+//int dy[]={ 0,1,0,-1};
+int main()
+{
+    //readf
+    int t , n , res , i , c = 0;
+    char s[100];
+    scanf("%d",&t);
+    wtm
+    {
+        scanf("%d %s",&n,s);
+        res = i = 0;
+        while(i<n)
+        {
+            if(s[i]=='.')
+            {
+                res++;
+                i+=2;
+            }
+            i++;
+        }
+        printf("Case %d: %d\n",++c,res);
+    }
+}
+
